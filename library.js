99795fedd5c9e70ae879c305ae5f950f7ce41aa1
CalAnizoba <CalAnizoba@example.com>
2022-03-08 19:17:30 +0800

    fixed the deletebtn bug and used a card instead of table

--- library.js
+++ library.js
@@ -1,104 +1,93 @@
-const addButton = document.querySelector('#newbook');
-const addform = document.getElementById('addbutton');
-const container = document.getElementById('container');
 const submitbtn = document.getElementById('submit');
-const table = document.getElementById('table');
-const library = [];
+const openbtn = document.getElementById('addbtn');
+const container = document.getElementById('form-container');
+const folder = document.getElementById('folder'); 
+const title = document.getElementById('fname').value;
+const author = document.getElementById('lname').value;
+const pages = document.getElementById('number').value;
+const read = document.getElementById('read').value;
+
+
+const myLibrary = [];
 let nextBookId = 1;
-let read = true;
 
-
-function Book(title, author, pages, read, id) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.id = id;
+function Book(title,author,pages,read,id) {
+     this.title = title;
+     this.author= author;
+     this.pages = pages;
+     this.read =  read;
+     this.id = id;   
 }
 
-function addBookToLibrary() {
+function addBookToLibrary(){
 
-    const title = document.getElementById('fname').value;
-    const author = document.getElementById('lname').value;
-    const pages = document.getElementById('number').value;
-    const read = document.getElementById('read').value;
-
-    books = new Book(title, author, pages, read, nextBookId);
-    library.push(books);
-    createTable();
+    let book = new Book(title,author,pages,read, nextBookId);
+    myLibrary.push(book);
+    // console.log(myLibrary);
+    Card();
     nextBookId++;
-}
-
-function createTable() {
-    var html = "<table border = '1|1'>";
-
-    html += '<tr>';
-    html += '<th>' + `TITLE` + '</th>';
-    html += '<th>' + `AUTHOR` + '</th>';
-    html += '<th>' + `PAGES` + '</th>';
-    html += '<th>' + `HAVE YOU READ IT?` + '</th>';
-    html += '<th>' + `Remove Row` + '</th>';
-
-    for (var i = 0; i < library.length; i++) {
-        html += `<tr data-id='${library[i].id}' id=removeRows'>`;
-        html += '<td>' + library[i].title + '</td>';
-        html += '<td>' + library[i].author + '</td>';
-        html += '<td>' + library[i].pages + '</td>';
-        html += '<td>' + library[i].read + '</td>';
-        html +=
-            `<td>
-            <button id=${library[i].id} class=removebtn onclick= removeRow(this.id)>Delete</button> 
-            </td>`;
-
-
-        html += '</tr>';
-
-    }
-   
-    html += '</table>';
-    document.getElementById('box').innerHTML = html;
-}
-
-
-function openForm(){
-
-    if (container.style.display !== 'none') {
-        container.style.display = 'block';
-    } else {
-        container.style.display = 'block';
-    }
-}
-
-
-
-function submitForm() {
-    document.getElementById('box').style.display = 'block';
-    addBookToLibrary();
     container.style.display = 'none';
 }
 
- function removeRow(id){
-    removeBook();
-    const row = document.getElementById(id).parentNode.parentNode;
-    row.remove(); 
- }
- function removeBook() {
-    index = `${library.id}`;
-    library.splice(index, 1);
+
+function Card(){
+    const readbtn = document.createElement('button');
+    const deletebtn = document.createElement('button');
+    let title = document.getElementById('fname').value;
+    let author = document.getElementById('lname').value;
+    let pages = document.getElementById('number').value;
+    let read = document.getElementById('read').value;
+    let box = document.createElement('box');
+    let titleholder = document.createElement('h1');
+    let authorholder = document.createElement('p');
+    let pagesholder = document.createElement('p');
+   
+    box.style.padding = '10px';
+    box.style.alignItems = 'center';
+
+    
+    titleholder.textContent = `Title : ${title}`;
+    authorholder.textContent = `Author : ${author}`;
+    pagesholder.textContent =  `Pages : ${pages}`;
+    readbtn.textContent = `Read: ${read}`;
+    deletebtn.textContent = `Delete`;
+    
+    folder.append(box);
+
+    console.log(myLibrary.length);
+    box.setAttribute('id', myLibrary.length);
+
+    // readbtn.setAttribute('id', myLibrary.length);
+   
+    box.append(titleholder);
+    box.append(authorholder);
+    box.append(pagesholder);
+    box.append(readbtn);
+    box.append(deletebtn); 
+
+    deletebtn.addEventListener('click', e =>{
+        folder.removeChild(box);
+        index = e.target.id;
+        myLibrary.splice(index, 1);
+    
+    });
+
+    readbtn.addEventListener('click', e =>{
+        read = e.target.textContent;
+        read == !read;
+    });
+}
+
+
+function addForm(){
+    if(container !== 'none'){
+        container.style.display = 'block';
+    }else{
+        container.style.display = 'block';
     }
-
-Book.prototype.checkbox = function(){
-   this.read = !this.read;
-};
-
-addform.addEventListener('click', openForm);
-submitbtn.addEventListener('click', submitForm);
-                
+}
 
 
 
-
-
-
-
-
+openbtn.addEventListener('click', addForm);
+submitbtn.addEventListener('click', addBookToLibrary);
