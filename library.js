8fd2682a592e190ce59839ceaf990947054075c5
CalAnizoba <obinnaanizoba5@gmail.com>
2022-03-08 23:44:37 +0800

    added some dom stylings to my box

--- library.js
+++ library.js
@@ -9,7 +9,10 @@ const read = document.getElementById('read').value;
 
 
 const myLibrary = [];
+
+//id starts at 1 and increments when an object has been created
 let nextBookId = 1;
+
 
 function Book(title,author,pages,read,id) {
      this.title = title;
@@ -20,70 +23,126 @@ function Book(title,author,pages,read,id) {
 }
 
 function addBookToLibrary(){
-
+    //creating instances
     let book = new Book(title,author,pages,read, nextBookId);
+    console.log(myLibrary);
+    //adds instance book into library []
     myLibrary.push(book);
-    // console.log(myLibrary);
-    Card();
+ 
+    //calls the Card function
+    createCard();
+
+    //increments the id anytime a book is created.. this is for indexing each book
     nextBookId++;
+
+    //form container display is off so we can only view the card when submitbtn has been pressed
     container.style.display = 'none';
+
+    //set box display to grid
+    // box.style.display = 'grid';
+   
+    // box.setAttribute('style', 'display:grid; grid-template-rows:repeat(4, 1fr); gap:10px; justify-content:center; align-items:center; ');
+
 }
 
 
-function Card(){
+function createCard(){
     const readbtn = document.createElement('button');
     const deletebtn = document.createElement('button');
     let title = document.getElementById('fname').value;
     let author = document.getElementById('lname').value;
     let pages = document.getElementById('number').value;
     let read = document.getElementById('read').value;
+
+
     let box = document.createElement('box');
     let titleholder = document.createElement('h1');
     let authorholder = document.createElement('p');
     let pagesholder = document.createElement('p');
    
-    box.style.padding = '10px';
-    box.style.alignItems = 'center';
 
+    //stye h1 tag
     
+    //style box
+    box.style.padding = '10px';
+    box.style.border = 'solid white 2px';
+    // box.style.width ='50vh';
+    // box.style.boxShadow = 20px, 20px, 10px, grey;
+
+    //set style for delete button
+    deletebtn.style.margin = '10px';
+    deletebtn.style.padding = '10px'
+    deletebtn.style.backgroundColor = '#CC9544';
+    deletebtn.style.backgroundColor = 'hover : #CC9544';
+
+    //set style for read button
+    readbtn.style.padding = '10px';
+    readbtn.style.backgroundColor = '#CC9544';
+    
+
+    //includes our form values into each tag inside box
     titleholder.textContent = `Title : ${title}`;
     authorholder.textContent = `Author : ${author}`;
     pagesholder.textContent =  `Pages : ${pages}`;
     readbtn.textContent = `Read: ${read}`;
     deletebtn.textContent = `Delete`;
     
+    //inserts box into our main container folder
     folder.append(box);
 
     console.log(myLibrary.length);
+    //sets box id to array length so we can get index of each box
     box.setAttribute('id', myLibrary.length);
 
-    // readbtn.setAttribute('id', myLibrary.length);
+    //sets box class to card
+    box.setAttribute('class', 'card');
+
+ 
    
+    //puts content inside box/card
     box.append(titleholder);
     box.append(authorholder);
     box.append(pagesholder);
     box.append(readbtn);
     box.append(deletebtn); 
 
+    //deletebtn
     deletebtn.addEventListener('click', e =>{
+        //removes box div from folder when deletebtn has been clicked
         folder.removeChild(box);
+
+        //gets index of the object id which has been clicked and is stored in index
         index = e.target.id;
+
+        //removes the object from the array
         myLibrary.splice(index, 1);
     
     });
 
-    readbtn.addEventListener('click', e =>{
-        read = e.target.textContent;
-        read == !read;
+    //readbutton
+    readbtn.addEventListener('click', () =>{
+      if(read = false){
+          read = true;
+      }else{
+          read = false;
+      }
     });
 }
 
-
+//Gets form and allows user to input something inside
 function addForm(){
+
     if(container !== 'none'){
         container.style.display = 'block';
+        // container.style.justifyContent = 'center';
+        // container.style.gridTemplateRows = '1fr 1fr';
+        // container.style.padding = '10px';
+        // container.style.alignItems = 'center';
     }else{
-        container.style.display = 'block';
+        container.style.display = 'grid';
+        // container.style.padding = '10px';
+        container.style.justifyContent = 'center';
+        // container.style.alignItems = 'center';
     }
 }
 
