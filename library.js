797b3b37409616ba94e70ce6a865793d7cca0f28
CalAnizoba <obinnaanizoba5@gmail.com>
2022-03-09 19:18:30 +0800

    fixed bug issues with getting form values

--- library.js
+++ library.js
@@ -2,10 +2,10 @@ const submitbtn = document.getElementById('submit');
 const openbtn = document.getElementById('addbtn');
 const container = document.getElementById('form-container');
 const folder = document.getElementById('folder'); 
-const title = document.getElementById('fname').value;
-const author = document.getElementById('lname').value;
-const pages = document.getElementById('number').value;
-const read = document.getElementById('read').value;
+const title = document.getElementById('fname')
+const author = document.getElementById('lname')
+const pages = document.getElementById('number')
+const read = document.getElementById('read')
 
 
 const myLibrary = [];
@@ -18,16 +18,18 @@ function Book(title,author,pages,read,id) {
      this.title = title;
      this.author= author;
      this.pages = pages;
-     this.read =  read;
-     this.id = id;   
+     this.read = read;
+     this.id = id;       
 }
+
 
 function addBookToLibrary(){
     //creating instances
-    let book = new Book(title,author,pages,read, nextBookId);
+    let book = new Book(title.value, author.value, pages.value, read.value, nextBookId);
     console.log(myLibrary);
     //adds instance book into library []
     myLibrary.push(book);
+   
  
     //calls the Card function
     createCard();
@@ -38,42 +40,30 @@ function addBookToLibrary(){
     //form container display is off so we can only view the card when submitbtn has been pressed
     container.style.display = 'none';
 
-    //set box display to grid
-    // box.style.display = 'grid';
-   
-    // box.setAttribute('style', 'display:grid; grid-template-rows:repeat(4, 1fr); gap:10px; justify-content:center; align-items:center; ');
-
 }
 
 
 function createCard(){
     const readbtn = document.createElement('button');
     const deletebtn = document.createElement('button');
-    let title = document.getElementById('fname').value;
-    let author = document.getElementById('lname').value;
-    let pages = document.getElementById('number').value;
-    let read = document.getElementById('read').value;
-
-
     let box = document.createElement('box');
     let titleholder = document.createElement('h1');
     let authorholder = document.createElement('p');
     let pagesholder = document.createElement('p');
    
 
-    //stye h1 tag
+
     
     //style box
     box.style.padding = '10px';
     box.style.border = 'solid white 2px';
-    // box.style.width ='50vh';
-    // box.style.boxShadow = 20px, 20px, 10px, grey;
+ 
 
     //set style for delete button
     deletebtn.style.margin = '10px';
     deletebtn.style.padding = '10px'
     deletebtn.style.backgroundColor = '#CC9544';
-    deletebtn.style.backgroundColor = 'hover : #CC9544';
+  
 
     //set style for read button
     readbtn.style.padding = '10px';
@@ -81,16 +71,17 @@ function createCard(){
     
 
     //includes our form values into each tag inside box
-    titleholder.textContent = `Title : ${title}`;
-    authorholder.textContent = `Author : ${author}`;
-    pagesholder.textContent =  `Pages : ${pages}`;
-    readbtn.textContent = `Read: ${read}`;
+    titleholder.textContent = `Title : ${Title.value}`;
+    authorholder.textContent = `Author : ${Author.value}`;
+    pagesholder.textContent =  `Pages : ${Pages.value}`;
+    readbtn.textContent = `Read: ${Read.value}`;
     deletebtn.textContent = `Delete`;
     
     //inserts box into our main container folder
     folder.append(box);
 
     console.log(myLibrary.length);
+
     //sets box id to array length so we can get index of each box
     box.setAttribute('id', myLibrary.length);
 
@@ -106,6 +97,7 @@ function createCard(){
     box.append(readbtn);
     box.append(deletebtn); 
 
+
     //deletebtn
     deletebtn.addEventListener('click', e =>{
         //removes box div from folder when deletebtn has been clicked
@@ -119,34 +111,19 @@ function createCard(){
     
     });
 
-    //readbutton
-    readbtn.addEventListener('click', () =>{
-      if(read = false){
-          read = true;
-      }else{
-          read = false;
-      }
-    });
 }
+
 
 //Gets form and allows user to input something inside
 function addForm(){
 
     if(container !== 'none'){
         container.style.display = 'block';
-        // container.style.justifyContent = 'center';
-        // container.style.gridTemplateRows = '1fr 1fr';
-        // container.style.padding = '10px';
-        // container.style.alignItems = 'center';
     }else{
         container.style.display = 'grid';
-        // container.style.padding = '10px';
         container.style.justifyContent = 'center';
-        // container.style.alignItems = 'center';
     }
 }
-
-
 
 openbtn.addEventListener('click', addForm);
 submitbtn.addEventListener('click', addBookToLibrary);
